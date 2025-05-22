72d162860e73325fc128943d564ebd7c41380d72
Ole von Klitzing <ovonklit@uni-bremen.de>
2025-05-22 07:30:44 +0200

    Added not working idea

--- H2O_test_TBMaLT/test.py
+++ H2O_test_TBMaLT/test.py
@@ -55,29 +55,46 @@ r_feed = RepulsiveSplineFeed.from_database(parameter_db_path, species)
 original_h = h_feed._on_sites["1"].clone()
 original_o = h_feed._on_sites["8"].clone()
 
-#torch.manual_seed(4)  # Set random seed for reproducibility
+torch.manual_seed(4)  # Set random seed for reproducibility
+
+# Create parameters directly in the on_sites dictionaries
+# For hydrogen (Z=1)
+h_onsite = h_feed._on_sites["1"].clone()
+h_onsite[0] = -torch.rand(1, dtype=torch.double, requires_grad=True)[0]  # x parameter
+#h_onsite[1:4] = -torch.rand(1, dtype=torch.double, requires_grad=True)[0].repeat(3)  # y parameter
+h_feed._on_sites["1"] = h_onsite
+
+# For oxygen (Z=8)
+o_onsite = h_feed._on_sites["8"].clone()
+o_onsite[0] = -torch.rand(1, dtype=torch.double, requires_grad=True)[0]  # a parameter
+o_onsite[1:4] = -torch.rand(1, dtype=torch.double, requires_grad=True)[0].repeat(3)  # b parameter
+h_feed._on_sites["8"] = o_onsite
+
+# Get the parameter references for optimization
+params = []
+for key in h_feed._on_sites:
+    h_feed._on_sites[key].requires_grad_(True)
+    params.append(h_feed._on_sites[key])
+
+# Create 3 individual trainable parameters
+#h_param = nn.Parameter(-torch.rand(1, dtype=torch.double))      # For H on-site[0]
+#o_param_a = nn.Parameter(-torch.rand(1, dtype=torch.double))    # For O on-site[0]
+#o_param_b = nn.Parameter(-torch.rand(1, dtype=torch.double))    # For O on-site[1:4]
 
 # Create parameters directly in the on_sites dictionaries
 # For hydrogen (Z=1)
 #h_onsite = h_feed._on_sites["1"].clone()
-#h_onsite[0] = -torch.rand(1, dtype=torch.double, requires_grad=True)[0]  # x parameter
-#h_onsite[1:4] = -torch.rand(1, dtype=torch.double, requires_grad=True)[0].repeat(3)  # y parameter
+#h_onsite[0] = h_param.squeeze()  # x parameter
 #h_feed._on_sites["1"] = h_onsite
 
 # For oxygen (Z=8)
 #o_onsite = h_feed._on_sites["8"].clone()
-#o_onsite[0] = -torch.rand(1, dtype=torch.double, requires_grad=True)[0]  # a parameter
-#o_onsite[1:4] = -torch.rand(1, dtype=torch.double, requires_grad=True)[0].repeat(3)  # b parameter
+#o_onsite[0] = o_param_a.squeeze()  # a parameter
+#o_onsite[1:4] = o_param_b.squeeze()  # b parameter (same value for positions 1, 2, 3)
 #h_feed._on_sites["8"] = o_onsite
 
-# Get the parameter references for optimization
-#params = []
-#for key in h_feed._on_sites:
-#    h_feed._on_sites[key].requires_grad_(True)
-#    params.append(h_feed._on_sites[key])
-
-
-
+# Get the 3 specific parameters for optimization
+params = [h_param, o_param_a, o_param_b]
 # Create the DFTB calculator
 dftb_calculator = Dftb2(h_feed, s_feed, o_feed, u_feed, r_feed, filling_scheme=None)
 
